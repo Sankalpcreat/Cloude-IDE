cac9695111e4a10faf13b32eb918ce2ebb2ef5e8
Sankalp Singh <singhsankalp997@gmail.com>
2024-08-26 14:50:16 +0530

    first commit

---
+++ client/src/components/terminal.jsx
@@ -0,0 +1,46 @@
+import { Terminal as XTerminal } from "@xterm/xterm";
+import { useEffect, useRef } from "react";
+import socket from "../socket";
+
+import "@xterm/xterm/css/xterm.css";
+
+const Terminal = () => {
+  const terminalRef = useRef();
+  const isRendered = useRef(false);
+
+  useEffect(() => {
+    if (isRendered.current) return;
+    isRendered.current = true;
+
+    const term = new XTerminal({
+      rows: 20,
+    });
+
+    term.open(terminalRef.current);
+
+    term.onData((data) => {
+      socket.emit("terminal:write", data);
+    });
+
+    function onTerminalData(data) {
+      term.write(data);
+    }
+
+    socket.on("terminal:data", onTerminalData);
+  }, []);
+
+  return (
+    <div className="terminal-container">
+      <div className="fakeMenu">
+        <div className="fakeButtons"></div>
+        <div className="fakeButtons fakeMinimize"></div>
+        <div className="fakeButtons fakeZoom"></div>
+      </div>
+      <div className="fakeScreen">
+        <div ref={terminalRef} id="terminal"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Terminal;
